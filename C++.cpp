c59d19b45651de7cb7d983e21e41ed814a3d931a
RishabhRaval <103925475+RishabhRaval@users.noreply.github.com>
2022-04-20 04:00:14 -0700

    initial commit

---
+++ C++.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int d,m,y; 
+    cout << "Enter date :";
+    cin >> d;
+    cout << "Enter month:";
+    cin >> m;
+    cout << "Enter year :";
+    cin >> y;
+    cout << "\nDATE OF BIRTH :" << d << m << y;
+    return 0;
+}
