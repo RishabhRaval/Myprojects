1b108e225306219f5135c725ee11617921da83e7
RishabhRaval <103925475+RishabhRaval@users.noreply.github.com>
2022-04-29 08:37:34 -0700

    Add files via upload

--- C++.cpp
+++ C++.cpp
@@ -1,15 +1,50 @@
 #include<iostream>
 using namespace std;
-
 int main()
 {
-    int d,m,y; 
-    cout << "Enter date :";
-    cin >> d;
-    cout << "Enter month:";
-    cin >> m;
-    cout << "Enter year :";
-    cin >> y;
-    cout << "\nDATE OF BIRTH :" << d << m << y;
+    int a,b,c;
+    cout << "\n\nEnter first number :";
+    cin >> a;
+    cout << "\nEnter second number :";
+    cin >> b;
+
+    cout << "\n**********MAIN MENU**********";
+    cout << "\n[1]ADDITION";
+    cout << "\n[2]SUBTRACTION";
+    cout << "\n[3]MULTIPLICATION";
+    cout << "\n[4]DIVSION(Quotient)";
+    cout << "\n[5]EXIT";
+    bjump:
+    cout << "\n\nCHOOSE YOUR OPTION :";
+    cin >> c;
+ 
+   switch(c)
+    {
+        case 1:
+        cout << "\nAddition : " << a+b;
+        break;
+
+        case 2:
+        cout << "\nSubtraction : " << a-b;
+        break;
+
+        case 3:
+        cout << "\nMultiplication : " << a*b;
+        break;
+
+        case 4:
+        cout << "\nDivision : " << a/b;
+        break;
+
+        if(c==5)
+        case 5:
+        cout << "\nGoodbye !!!!";
+        break;
+        
+        default:
+        cout << "\nEnter valid input(choice) !!";
+    }
+    if(c!=5)
+        goto bjump;
     return 0;
 }
